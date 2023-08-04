a3a015e7b2cc7f3aef1ea904e8461ee4747fff54
Gustavo <incio.gusta@gmail.com>
2023-08-04 13:59:33 -0300

    update lifetimes

--- src/schema.rs
+++ src/schema.rs
@@ -1,14 +1,14 @@
 use crate::Row;
 
 pub trait Schema {
-    fn get_columns(&self) -> &[&'static str];
+    fn get_columns<'a>(&self) -> &[&'a str];
 }
 
 impl<T> Schema for T
 where
     T: Row,
 {
-    fn get_columns(&self) -> &[&'static str] {
+    fn get_columns<'a>(&self) -> &[&'a str] {
         T::COLUMN_NAMES
     }
 }
